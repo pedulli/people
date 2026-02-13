c82ce0624b93769fef4a15b5a12b87d2d5f80c6e
pedulli <t@mail.rit.edu>
2026-02-12 22:17:37 -0500

    e5

--- www/iste-240/knickknacks/manuscripts/e5.js
+++ www/iste-240/knickknacks/manuscripts/e5.js
@@ -8,12 +8,17 @@ const $ = // Offbrand jQuery
 
 
 SectionA: {
-	const buttonElement = $('#sectionA > button')
+	const buttonElement = $('#changeStyleButton')
+	const moveButtonElement = $('#moveTextButton')
 	const textElement = $('#sectionA > p')
 
-	buttonElement.addEventListener('click', () => {
-		textElement.classList.toggle('green')
-	})
+	buttonElement.addEventListener('click', () =>
+		textElement.classList.toggle('green'))
+
+	moveButtonElement.addEventListener('click', () =>
+		textElement.style.translate = CSS.px(
+			parseInt(textElement.style.translate || 0) + 10
+		))
 }
 
 
@@ -21,32 +26,43 @@ SectionB: {
 	const formElement = $('#sectionB > form')
 
 	formElement.addEventListener('submit', (event) => {
-		const [firstName, lastName] = new FormData(event.target).values()
-
-		alert(firstName + ' ' + lastName)
-
 		event.preventDefault()
+		if (event.submitter.id)
+			return howmany($('#sectionB'))
+
+		const [firstName, lastName] = new FormData(event.target).values()
+		alert(firstName + ' ' + lastName)
 	})
+
+	function howmany(parentElement) {
+		const inputElements = parentElement.querySelectorAll('input')
+		const textInputElements = [...inputElements].filter(({ type }) => type === 'text')
+
+		console.log(`Number of total inputs: ${inputElements.length}`)
+		console.log(`Number of text inputs: ${textInputElements.length}`)
+	}
 }
 
 
 SectionC: {
+	const parentElement = $('#sectionC')
 	const buttonElement = $('#sectionC > button')
 	const selectElement = $('#sectionC > select')
 
 	buttonElement.addEventListener('click', () =>
-		alert(selectElement.options.length))
+		parentElement.style.backgroundColor = selectElement.value)
 }
 
 
 SectionD: {
+	const selectElement = $('#sectionC > select')
 	const textElement = $('#sectionD > p')
 
 	textElement.addEventListener('mouseenter', () =>
-		textElement.classList.add('green'))
+		textElement.style.color = selectElement.value)
 
 	textElement.addEventListener('mouseleave', () =>
-		textElement.classList.remove('green'))
+		textElement.style.color = 'black')
 }
 
 
@@ -55,11 +71,10 @@ SectionE: {
 	const textElement = $('#sectionE span')
 
 	formElement.addEventListener('submit', (event) => {
+		event.preventDefault()
 		const [first, second] = new FormData(event.target).values()
 
-		textElement.innerText = // I assure you I made sure this is safe
+		textElement.innerText = // I assure you I made sure this eval is safe
 			eval?.(first + event.submitter.dataset.operator + second)
-
-		event.preventDefault()
 	})
 }
