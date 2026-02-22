e47a77d6ee03220808a4d130cf678b5b61ccf8db
pedulli <t@mail.rit.edu>
2026-02-22 15:55:34 -0500

    optimise & preload

--- www/iste-240/knickknacks/manuscripts/e6.js
+++ www/iste-240/knickknacks/manuscripts/e6.js
@@ -1,4 +1,3 @@
-const imageSrc = (name, thumb = false) => `knickknacks/depictions/e6/${name + (thumb ? '.thumb' : '')}.jpg`
 const thumbnailListElement = document.querySelector('ul')
 const imageElement = document.querySelector('figure > img')
 const captionElement = document.querySelector('figcaption')
@@ -11,24 +10,30 @@ const images = {
 	river: 'River going through valley',
 	swan: 'Swan with reflextion on lake',
 	waterfall: 'Very nice waterfall',
-	lizard: 'Awesome gecko'
+	lizard: 'Awesome gecko',
 }
 
 for (const image in images) {
-	const button = document.createElement('button')
-	const listItem = document.createElement('li')
-	const thumbnail = document.createElement('img')
+	const buttonElement = document.createElement('button')
+	const listItemElement = document.createElement('li')
+	const thumbnailElement = document.createElement('img')
+	const preloadElement = new Image
 
-	thumbnail.src = imageSrc(image, true)
-	thumbnail.alt = `switch to "${images[image]}" image`
-	button.appendChild(thumbnail)
-	listItem.appendChild(button)
-	thumbnailListElement.appendChild(listItem)
+	thumbnailElement.src = `knickknacks/depictions/e6/${image}.thumb.jpg`
+	thumbnailElement.alt = `switch to "${images[image]}" image`
+	thumbnailElement.draggable = false
+	preloadElement.src = `knickknacks/depictions/e6/${image}.jpg`
+	buttonElement.appendChild(thumbnailElement)
+	listItemElement.appendChild(buttonElement)
+	thumbnailListElement.appendChild(listItemElement)
 
-	button.addEventListener('click', () => {
-		imageElement.src = imageSrc(image)
+	buttonElement.addEventListener('mousedown', swap)
+	buttonElement.addEventListener('click', swap)
+
+	function swap() {
+		imageElement.src = preloadElement.src
 		captionElement.textContent = images[image]
-	})
+	}
 }
 
 document.querySelector('button').click()
